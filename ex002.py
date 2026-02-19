916d1a000bdb82a44b68117ee8a87496c41a7aba
carolinaropeDev <carolinarodriguespereira21@gmail.com>
2026-02-19 14:24:41 -0300

    fiz o Exercicio 2

---
+++ ex002.py
@@ -0,0 +1,7 @@
+# EXERCÍCIO 02
+# Este exercício ajuda a entender como ler algo que o usuário digita.
+# O 'input' faz uma pergunta e o que a pessoa responder fica guardado na variável 'nome'.
+nome = input('Qual é o seu nome? ')
+
+# O 'print' mostra a saudação personalizada na tela.
+print('É um prazer te conhecer,', nome)
