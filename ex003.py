2374755d05d7b9f8e8d277bf65e8cddb810fe480
carolinaropeDev <carolinarodriguespereira21@gmail.com>
2026-02-19 15:15:23 -0300

    Exercicio 3
    soma de dois valores preenchidos pelo usuario

---
+++ ex003.py
@@ -0,0 +1,5 @@
+#Exercício Python 003: Crie um programa que leia dois números e mostre a soma entre eles.
+print('\033[36m Digite 2 números e direi a soma deles \033[m')
+n1=int(input('Digite um número :'))
+n2=int(input('Digite outro número :'))
+print(f'Você digitou os números {n1} e {n2} \n A soma deles é {n1+n2}')
